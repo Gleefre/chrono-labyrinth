4ea16c35d92280a161eb6f11631628f6f0032c38
Gleefre <varedif.a.s@gmail.com>
2023-10-30 00:36:06 +0100

    Rework saving / loading

--- src/save-world.lisp
+++ src/save-world.lisp
@@ -1,89 +1,79 @@
 (in-package #:chrono-labyrinth)
 
-;;; Copying, saving, loading.
+;;; Copying
 
-(defgeneric copy-game-object (object))
-(defgeneric object->list (object))
-(defgeneric tag-list->object (tag &rest args))
-(defun list->object (list)
-  (apply #'tag-list->object list))
+(defmethod copy-game-object (object)
+  (list->object (object->list object)))
 
-;; TODO: A limited version for the wall, ground, etc
-(defmacro gen-base-functions (object-class)
-  `(progn
-     (defmethod copy-game-object ((object ,object-class))
-       (make-instance ',object-class
-                      :position (copy-list (object-position object))
-		      :layer (object-layer object)))
+;;; Converting to list
 
-     (defmethod object->list ((object ,object-class))
-       (list ,(make-keyword object-class)
-	     :position (copy-list (object-position object))
-	     :layer (object-layer object)))
+(defmethod object->list (object)
+  (list* (object->tag object)
+         (object->untagged-list object)))
 
-     (defmethod tag-list->object ((tag (eql ,(make-keyword object-class))) &key position layer)
-       (make-instance ',object-class
-		      :position (copy-list position)
-		      :layer layer))))
+(defgeneric object->tag (object))  ; defined below
 
-(gen-base-functions box)
-(gen-base-functions wall)
-(gen-base-functions semi-wall)
-(gen-base-functions hourglass-1)
-(gen-base-functions hourglass-2)
-(gen-base-functions ground)
-(gen-base-functions water)
-(gen-base-functions empty)
+(defmethod object->untagged-list append ((world world))
+  (list :time-flow (world-time-flow world)
+        :objects (mapcar #'object->list (reverse (world-objects world)))))
 
-;; game-block
-(defmethod copy-game-object ((object game-block))
-  (make-instance 'game-block
-                 :position (copy-list (object-position object))
-                 :offset (block-offset object)
-                 :on-cancel (block-on-cancel object)
-                 :program (a:copy-array (block-program object))))
+(defgeneric object->untagged-list (object)
+  (:method-combination append))
 
-(defmethod object->list ((object game-block))
-  (list :game-block
-        :position (copy-list (object-position object))
-        :offset (block-offset object)
+(defmethod object->untagged-list append ((object game-object))
+  (list :position (copy-list (object-position object))))
+
+(defmethod object->untagged-list append ((object game-block))
+  (list :offset (block-offset object)
         :on-cancel (block-on-cancel object)
         :program (coerce (block-program object) 'list)))
 
-(defmethod tag-list->object ((tag (eql :game-block)) &key position offset on-cancel program)
-  (make-instance 'game-block
-                 :position (copy-list position)
-                 :offset offset
-                 :on-cancel on-cancel
-                 :program (coerce program 'vector)))
+(defmethod object->untagged-list append ((player player))
+  (list :action (player-action player)))
 
-;; player
-(defmethod copy-game-object ((object player))
-  (make-instance 'player
-                 :position (copy-list (object-position object))
-                 :action (player-action object)))
+(defmethod object->untagged-list append ((hourglass hourglass))
+  (list :charged (hourglass-charged hourglass)))
 
-(defmethod object->list ((object player))
-  (list :player
-        :position (copy-list (object-position object))
-        :action (player-action object)))
+;;; Converting from list
 
-(defmethod tag-list->object ((tag (eql :player)) &key position action)
-  (make-instance 'player
-                 :position (copy-list position)
-                 :action action))
+(defun list->object (list)
+  (apply #'tag-list->object list))
+
+(defmethod tag-list->object (tag &rest args)
+  (apply #'make-instance (tag->class tag)
+         (loop for (name thing) on args by #'cddr
+               collect name collect (field->initarg name thing))))
+
+(defmethod tag->class (tag) 'game-object)  ; defined below
+
+(defmethod field->initarg (name thing) thing)
+
+(defmethod field->initarg ((name (eql :position)) position)
+  (copy-list position))
+
+(defmethod field->initarg ((name (eql :program)) program)
+  (coerce program 'vector))
+
+;;; Trivial methods for tag->class and object->tag
+
+(macrolet ((defmethods (&rest classes)
+               `(progn
+                  ,@(loop for object-class in classes
+                          for tag = (a:make-keyword object-class)
+                          collect `(defmethod object->tag ((object ,object-class)) ,tag)
+                          collect `(defmethod tag->class ((tag (eql ,tag))) ',object-class)))))
+  (defmethods
+    game-block
+    player
+    box
+    ground
+    wall
+    semi-wall
+    hourglass
+    level-exit
+    world))
 
 ;; world
-(defmethod copy-game-object ((object world))
-  (let ((copy (make-instance 'world :time-flow (world-time-flow object))))
-    (dolist (world-object (reverse (world-objects object)) copy)
-      (add-to-world (copy-game-object world-object) copy))))
-
-(defmethod object->list ((object world))
-  (list :world
-        :time-flow (world-time-flow object)
-        :objects (mapcar #'object->list (reverse (world-objects object)))))
-
 (defmethod tag-list->object ((tag (eql :world)) &key objects time-flow)
   (let ((world (make-instance 'world :time-flow time-flow)))
     (dolist (world-object (mapcar #'list->object objects) world)
