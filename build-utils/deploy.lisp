3b3257c55e831198835432d2e0aa48e5d8f10bdf
Gleefre <varedif.a.s@gmail.com>
2023-10-30 12:14:22 +0100

    Update deploy scripts

--- build-utils/deploy.lisp
+++ build-utils/deploy.lisp
@@ -22,6 +22,14 @@
 
 (dont-deploy
  cl-opengl-bindings::opengl
+ ;; Don't deploy audio backend libraries
+ #+linux   (org.shirakumo.fraf.mixed.pulse.cffi::libpulse-simple
+            org.shirakumo.fraf.mixed.pulse.cffi::libpulse
+            org.shirakumo.fraf.mixed.alsa.cffi::libasound)
+ #+windows (org.shirakumo.fraf.mixed.winmm.cffi::winmm
+            org.shirakumo.fraf.mixed.wasapi.cffi::avrt)
+ #+darwin  (org.shirakumo.fraf.mixed.coreaudio.cffi::audio-toolbox
+            org.shirakumo.fraf.mixed.coreaudio.cffi::audio-unit)
  ;; Can't ship SDL2 - too many dependencies
  ;; FIXME
  #+linux (sdl2::libsdl2
