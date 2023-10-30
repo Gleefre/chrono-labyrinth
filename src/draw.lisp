4424b788fa6dc07f4c89704ce34ded62c4059b1d
Gleefre <varedif.a.s@gmail.com>
2023-10-30 01:50:31 +0100

    Kind of add draw-world (still need to use camera / width / height though)

--- src/draw.lisp
+++ src/draw.lisp
@@ -1,5 +1,7 @@
 (in-package #:chrono-labyrinth)
 
+;;; Tilesets
+
 (defclass tileset ()
   ((filename   :initarg :filename   :accessor tileset-filename)
    (tile-side  :initarg :tile-side  :accessor tileset-tile-side)
@@ -43,6 +45,75 @@
                       :mag-filter mag-filter
                       :min-filter min-filter)
            x y side side))
+
+;;; Existing tilesets
+
+(defparameter +character-tileset+
+  (make-instance 'tileset
+                 :filename "textures/character.png"
+                 :tile-side 64
+                 :columns 4
+                 :count 4))
+
+(defparameter +world-tileset+
+  (make-instance 'tileset
+                 :filename "textures/tiles-min.png"
+                 :tile-side 32
+                 :columns 4
+                 :count 8))
+
+;;; Layers
+
+(defgeneric object-layer (object)
+  (:method ((thing game-block)) 3)
+  (:method ((thing player)) 3)
+  (:method ((thing box)) 3)
+  (:method ((thing ground)) 1)
+  (:method ((thing wall)) 1)
+  (:method ((thing semi-wall)) 1)
+  (:method ((thing hourglass)) 2)
+  (:method ((thing level-exit)) 1))
+
+(defun layered (objects)
+  (flet ((layer (id)
+           (remove-if-not (a:curry #'eql id) objects :key #'object-layer)))
+    (append (layer 1) (layer 2) (layer 3))))
+
+(defgeneric object->tile (object)
+  (:method ((thing game-block)) 5)
+  (:method ((thing box)) 1)
+  (:method ((thing ground)) 4)
+  (:method ((thing wall)) 6)
+  (:method ((thing semi-wall)) 2)
+  (:method ((thing hourglass))
+    (if (hourglass-charged thing)
+        3
+        7))
+  (:method ((thing player))
+    (case (player-action thing)
+      ((:stay :down) 0)
+      ((:up) 1)
+      ((:left) 2)
+      ((:right) 3))))
+
+(defmethod draw-object ((object game-object))
+  (tile (object->tile object)))
+
+(defmethod draw-object :around ((object player))
+  (let ((*tileset* +character-tileset+))
+    (call-next-method)))
+
+(defmethod draw-object :around ((object game-object))
+  (with-slots (position) object
+    (when position
+      (destructuring-bind (x y) position
+        (s:with-translate ((* (tileset-tile-side +world-tileset+) x)
+                           (* (tileset-tile-side +world-tileset+) y))
+          (call-next-method))))))
+
+(defun draw-world (width height camera &optional (world *world*))
+  (declare (ignorable width height camera))
+  (map nil #'draw-object (layered (world-objects world))))
 
 ;; TODO figure out sketch figures?
 (defun draw-clock (clock w h &aux (time (sc:time clock)))
